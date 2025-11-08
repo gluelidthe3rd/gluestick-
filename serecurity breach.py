f972527024000b4ccbe5882e9693406031a8f7b5
gluelidthe3rd <gluelidthe3rd@gmail.com>
2025-11-08 09:57:06 +0000

    Add files via upload

---
+++ serecurity breach.py
@@ -0,0 +1,30 @@
+import time
+
+passkey = ("tbjfrinoaegenjrg")
+ 
+print("Welcome to the wall !")
+print()
+print("We have to run some sercurity checks.")
+time.sleep(1.5)
+print("please hold")
+time.sleep(1.5)
+print("loading....")
+time.sleep(3)
+print ("Entry:True")
+print()
+time.sleep(1)
+print("Hey looks like you go in ! loading up the pannel now !")
+time.sleep(2)
+print()
+print()
+print()
+
+
+#p = input (" Enter the pass key -")
+#e = input (" Enter your favourite emoticon-")
+
+print("option1. Exit")
+print("option2. sercurity check")
+print("option3. Enter the file")
+q = input(" please choose a option -")
+
